c4ca5d9fe6fc11b99d9f1dce8ff35314ad831b6e
Jordao Serafim <jordaoesa@gmail.com>
2016-08-16 16:57:07 -0300

    asterisk

--- config_asterisk.sh
+++ config_asterisk.sh
@@ -102,31 +102,62 @@ exten => 2004,2,Record(ruim.gsm)
 exten => 2004,3,PlayBack(ruim)
 exten => 2004,4,Hangup
 
+exten => 2005,1,Answer
+exten => 2005,2,Record(beep.gsm)
+exten => 2005,3,PlayBack(beep)
+exten => 2005,4,Hangup
+
+exten => 2006,1,Answer
+exten => 2006,2,Record(finalizar.gsm)
+exten => 2006,3,PlayBack(finalizar)
+exten => 2006,4,Hangup
+
+exten => 2007,1,Answer
+exten => 2007,2,Record(escute.gsm)
+exten => 2007,3,PlayBack(escute)
+exten => 2007,4,Hangup
+
+exten => 2008,1,Answer
+exten => 2008,2,Record(avaliacao.gsm)
+exten => 2008,3,PlayBack(avaliacao)
+exten => 2008,4,Hangup
+
 ;----------------------------------------------
-;- Configuracao dos botoes para as gravacoes  -
+;-            Configuracao da URA             -
 ;----------------------------------------------
-; exten => 2000,Goto(ura,s,1)
-; - Ao ligar 2000 do X-LITE, o asterisk vai buscar o contexto chamado 'ura' 
+; exten => 2000,Goto(ura_dl,inicio_ura,1)
+; - Ao ligar 2000 do X-LITE, o asterisk vai buscar o contexto chamado 'ura_dl' 
 ; e executar os passos configurados ali.
-; - O 's' aqui usado refencia uma extensao que deve estar configurada no
+; - O 'inicio_ura' aqui usado refencia uma extensao que deve estar configurada no
 ; contexto da 'ura' e o 1 define a prioridade
 ; - A aplicacao Background(nome_som) tocara o som passado em parametro em backgroud
 ; - A aplicacao Goto pode ser utilizada sem passarmos o parametro de contexto (apenas extensoes e prioridade)
 ; - A aplicacao PlayBack executara o arquivo de som passado em parametro
 
-exten => 2000,Goto(ura,s,1)
-[ura]
-exten => s,1,Answer
-exten => s,2,BackGround(som_ura)
+exten => 2000,1,Goto(ura_dl,inicio_ura,1)
 
+[ura_dl]
+;exten => inicio_ura,1,Answer
+exten => inicio_ura,2,PlayBack(som_ura)
+exten => inicio_ura,3,PlayBack(beep)
+exten => inicio_ura,4,Record(som_gravado.gsm,0,5,n)
+exten => inicio_ura,5,PlayBack(escute)
+exten => inicio_ura,6,PlayBack(som_gravado)
+exten => inicio_ura,7,PlayBack(avaliacao)
+exten => inicio_ura,8,Goto(ura_feedback,1)
+
+[ura_feedback]
 exten => 1,1,PlayBack(bom)
-exten => 1,2,Goto(s,1)
+exten => 1,2,PlayBack(finalizar)
+exten => 1,3,Goto(inicio_ura,1)
 
 exten => 2,1,PlayBack(medio)
-exten => 2,2,Goto(s,1)
+exten => 2,2,PlayBack(finalizar)
+exten => 2,3,Goto(inicio_ura,1)
 
 exten => 3,1,PlayBack(ruim)
-exten => 3,2,Goto(s,1)
+exten => 3,2,PlayBack(finalizar)
+exten => 3,3,Goto(inicio_ura,1)
 
 
 " > /etc/asterisk/extensions.conf;
